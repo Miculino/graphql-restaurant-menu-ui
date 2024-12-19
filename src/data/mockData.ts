d0f108490e0bcff8c9d1bc24dfd6f6e37c504d18
RemusDB <remusd.buhaianu@gmail.com>
2024-12-19 22:54:04 +0200

    fix: add incremental flag to tsconfig and remove invalid option

---
+++ src/data/mockData.ts
@@ -0,0 +1,26 @@
+// Restaurant Menu
+export const RESTAURANT_MENU = {
+  id: "1",
+  label: "Mrs Kueh Menu",
+  start_date: "2024-01-01",
+  end_date: "2024-01-01",
+  state: "active",
+  sections: [
+    {
+      id: "1",
+      label: "Platters",
+      description: null,
+      items: [
+        {
+          id: "1",
+          label: "Mrs Kueh's Favourites",
+          description:
+            "\u003cp\u003eIncludes:\u003cbr/\u003e3 pieces of Kueh Salat\u003cbr/\u003e3 pieces of Bingka Ubi\u003cbr/\u003e3 pieces of Ondeh Ondeh\u003cbr/\u003e3 pieces of Putri Ayu\u003cbr/\u003eKueh Kosui\u003c/p\u003e\u003cp\u003e\u003c/p\u003e",
+          display_order: 0,
+          type: "Product",
+          price: 35.0,
+        },
+      ],
+    },
+  ],
+};
