2f57a749bb7caeab3c59db274e0c6b5aaac4fdb1
RemusDB <remusd.buhaianu@gmail.com>
2024-12-19 23:25:51 +0200

    feat: render mock menu items

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+// Components
+import Sidebar from "./components/Sidebar";
+import MenuItems from "./components/MenuItems";
+
+// Data
+import { RESTAURANT_MENU } from "./data/mockData";
+
 function App() {
-  return <main className="min-h-screen py-10 bg-background">Test</main>;
+  const menu = RESTAURANT_MENU;
+
+  return (
+    <main className="min-h-screen py-10 bg-background text-black flex gap-10 justify-between px-2 md:px-5 lg:px-10 xl:px-32">
+      <Sidebar sections={menu.sections} />
+      <MenuItems menu={menu} />
+    </main>
+  );
 }
 
 export default App;
--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,3 +1,41 @@
-export default function MenuItems() {
-  return <div>MenuItems</div>;
+interface MenuItem {
+  id: string;
+  label: string;
+  description: string | null;
+  display_order: number;
+  type: string;
+  price: number;
+}
+
+interface Section {
+  id: string;
+  label: string;
+  description: null | string;
+  items: MenuItem[];
+}
+
+interface Menu {
+  id: string;
+  label: string;
+  start_date: string;
+  end_date: string;
+  state: string;
+  sections: Section[];
+}
+
+export default function MenuItems({ menu }: { menu: Menu }) {
+  return (
+    <div className="w-3/4 bg-background text-black">
+      {menu.sections.map((section) => (
+        <div key={section.id}>
+          <h2 className="text-2xl font-display">{section.label}</h2>
+          {section.items.map((item) => (
+            <div key={item.id}>
+              <h3 className="text-xl font-display">{item.label}</h3>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
