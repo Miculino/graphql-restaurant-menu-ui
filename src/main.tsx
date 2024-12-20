6d0f397a31c9edd0bea6c3593b5cf4673cbdc532
RemusDB <remusd.buhaianu@gmail.com>
2024-12-20 09:49:41 +0200

    feat: configure cors for apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,29 @@ import "./index.css";
 import App from "./App.tsx";
 
 // Apollo
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+// Configure Apollo Client with custom HTTP link for CORS support
+const httpLink = createHttpLink({
+  uri:
+    import.meta.env.VITE_GRAPHQL_API_ENDPOINT ||
+    "http://localhost:4000/graphql",
+  credentials: "include",
+});
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_API_ENDPOINT,
+  link: httpLink,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "network-only",
+    },
+  },
 });
 
 createRoot(document.getElementById("root")!).render(
