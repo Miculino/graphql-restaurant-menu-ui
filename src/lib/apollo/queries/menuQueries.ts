33d4802530746d91db9323b8f5a38a96757ec8e6
RemusDB <remusd.buhaianu@gmail.com>
2024-12-20 18:38:37 +0200

    feat: add gql query for fetching menu data

---
+++ src/lib/apollo/queries/menuQueries.ts
@@ -0,0 +1,32 @@
+// GraphQL
+import { gql } from "@apollo/client";
+
+export const GET_MENUS = gql`
+  query Menus {
+    menus {
+      id
+    }
+  }
+`;
+
+export const GET_MENU = gql`
+  query Menu($id: ID!) {
+    menu(id: $id) {
+      id
+      sections {
+        id
+        label
+        display_order
+        items {
+          id
+          label
+          description
+          type
+          price
+          thumbnail_url
+          display_order
+        }
+      }
+    }
+  }
+`;
